769aae46a76d1a242625b68673a106f84a8048a6
Chien <haminhchien7979@gmail.com>
2025-09-10 14:56:22 +0700

    code html

---
+++ main.js
@@ -0,0 +1,26 @@
+
+//Toán tử ++ --
+console.log('Hậu tố')
+
+var number = 1 ;
+
+console.log(number++);
+console.log(number);
+
+var number2 = 1;
+
+console.log(++number2);
+
+//Type of data 
+
+var myfunction = function(){
+    // console.log("Tôi đang bắt đầu học lại lập trình");
+}
+myfunction();
+
+var object = {
+
+    name: "Chiến",
+    age : 21
+}
+// console.log(object);
